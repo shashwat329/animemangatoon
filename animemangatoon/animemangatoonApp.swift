e62fdc90d1809c879815f45b685b8e03a9e719ad
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-10-11 01:59:54 +0530

    project started

---
+++ animemangatoon/animemangatoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  animemangatoonApp.swift
+//  animemangatoon
+//
+//  Created by shashwat singh on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct animemangatoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ animemangatoonTests/animemangatoonTests.swift
@@ -0,0 +1,17 @@
+//
+//  animemangatoonTests.swift
+//  animemangatoonTests
+//
+//  Created by shashwat singh on 11/10/24.
+//
+
+import Testing
+@testable import animemangatoon
+
+struct animemangatoonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
