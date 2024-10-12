0a749b35a44b361df24bf0aeb05332577740bac2
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-10-12 11:16:27 +0530

    added rating functionality

---
+++ animemangatoon/View/StarRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  StarRatingView.swift
+//  animemangatoon
+//
+//  Created by shashwat singh on 12/10/24.
+//
+
+import SwiftUI
+
+struct StarRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarRatingView()
+}
